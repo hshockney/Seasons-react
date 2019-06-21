0ccfa39da957f4189b6d7e96a4825b374fc28a08
hshockney <shockney@atlanticbb.net>
2019-06-21 10:49:13 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+
+class App extends React.Component {
+    state = { lat: null, errormessage: '' };
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
+        );
+    }
+    
+    componentDidUpdate() {
+        console.log("My component rerendered due to updating");
+    }
+    
+    render() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <div> Error: { this.state.errorMessage }</div>;
+        }
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={ this.state.lat }/>
+        }
+        return <div>Loading...</div>;
+    }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
+
