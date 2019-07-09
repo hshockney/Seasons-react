1a8aa3ddc728625599c1b22bb2c07300e7d4d8fd
hshockney <shockney@atlanticbb.net>
2019-07-09 16:40:40 -0400

    Updated to use hooks

--- src/index.js
+++ src/index.js
@@ -1,35 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Spinner from './Spinner';
-class App extends React.Component {
-    state = { lat: null, errormessage: '' };
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude }),
-            err => this.setState({ errorMessage: err.message })
-        );
-    }
+
+const App = () => {
+    const [lat, setLat] = useState(null);
+    const [errorMessage, setErrorMessage] = useState('');
     
-    componentDidUpdate() {
-        console.log("My component rerendered due to updating");
-    }
-    renderContent() {
-        if(this.state.errorMessage && !this.state.lat) {
-            return <div> Error: { this.state.errorMessage }</div>;
-        }
-        if(!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={ this.state.lat }/>
-        }
-        return <div><Spinner message="Please accept location request"/></div>;
-    }
-    render() {
-        return (
-        <div className="">
-            {this.renderContent()}
-        </div>
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            position => setLat(position.coords.latitude),
+            err => setErrorMessage(err.message)
         );
+    }, []);
+    let content;
+    if(errorMessage){
+        content = <div>Error: {errorMessage}</div>;
+    } else if (lat) {
+        content = <SeasonDisplay lat={lat} />;
+    } else {
+        content = <div><Spinner message="Please accept location request"/></div>;
     }
+    return <div className="border red">{content}</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
